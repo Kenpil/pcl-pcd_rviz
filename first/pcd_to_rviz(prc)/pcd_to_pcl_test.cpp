8286ae2fd70ba82c09d53030b1621eacff8328a3
Kenpil <42032640+Kenpil@users.noreply.github.com>
2019-06-09 23:47:39 +0900

    Add files via upload

--- first/pcd_to_rviz(prc)/pcd_to_pcl_test.cpp
+++ first/pcd_to_rviz(prc)/pcd_to_pcl_test.cpp
@@ -6,6 +6,7 @@
 #include <pcl/point_types.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl_ros/io/pcd_io.h>
+#include <pcl/filters/voxel_grid.h>
 
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 using namespace std;
@@ -16,9 +17,11 @@ int main(int argc, char **argv)
     double y_cloud;
     double z_cloud;
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
     ros::init(argc, argv, "my_pcl_tutorial");
     ros::NodeHandle nh;
     ros::Publisher pub = nh.advertise<PointCloud>("points2", 1);
+    pcl::VoxelGrid<pcl::PointXYZ> sor;
 
     std_msgs::String filemsg;
     ros::NodeHandle pn("~");
@@ -28,22 +31,24 @@ int main(int argc, char **argv)
     float LeafSize;
     pn.getParam("downsampling_rate", LeafSize);
     ROS_INFO("LeafSize%f\n", LeafSize);
-
-    //pcl::io::loadPCDFile<pcl::PointXYZ>(filemsg.data.c_str(), *cloud);
     pcl::PCDReader reader;
     reader.read(filemsg.data.c_str(), *cloud);
 
+    sor.setInputCloud(cloud);
+    sor.setLeafSize(LeafSize, LeafSize, LeafSize);
+    //sor.setLeafSize(0.1, 0.1, 0.1);
+    sor.filter(*cloud_filtered);
 
     PointCloud::Ptr msg(new PointCloud);
     msg->header.frame_id = "map";
-    msg->height = cloud->height;
-    msg->width = cloud->width;
+    msg->height = cloud_filtered->height;
+    msg->width = cloud_filtered->width;
 
-    for (size_t i = 0; i < cloud->size(); i++)
+    for (size_t i = 0; i < cloud_filtered->size(); i++)
     {
-        x_cloud = cloud->points[i].x;
-        y_cloud = cloud->points[i].y;
-        z_cloud = cloud->points[i].z;
+        x_cloud = cloud_filtered->points[i].x;
+        y_cloud = cloud_filtered->points[i].y;
+        z_cloud = cloud_filtered->points[i].z;
         msg->points.push_back(pcl::PointXYZ(x_cloud, y_cloud, z_cloud));
     }
 
